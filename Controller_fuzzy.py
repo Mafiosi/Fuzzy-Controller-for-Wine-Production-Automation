e7a65e1f5a9fddc5871c0d909d03233d23576369
Joao Beleza <joao.beleza7@gmail.com>
2018-11-08 00:59:36 +0000

    VERSION 0.4     - ALL RULES ADDED     - Control Implementation

--- Controller_fuzzy.py
+++ Controller_fuzzy.py
@@ -6,103 +6,250 @@ import numpy as np
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import skfuzzy.control as control
+from queue import Queue
 
 ##############################
 ##     EOF  LIBRARIES       ##
 ##############################
-##############################
-##     INPUT VARIABLES      ##
-##############################
-
-#HOW MUCH TIME WE HAVE
-time_end = 15 + 1
 
 
-##############################
-##  EOF INPUT VARIABLES     ##
-##############################
-##############################
-##    FUZZY DEFINITION      ##
-##############################
-#NAME LIST
+# INITIALIZATION OF CONTROLLER
+def fuzzy_initialization(use_tanins):
 
-standard_in_3 = ['LOW', 'MEDIUM', 'HIGH']
-special_in_3 = ['BAD', 'MEDIUM', 'GOOD']
-standard_in_5 = ['BIG LOW', 'LOW', 'MEDIUM', 'HIGH', 'BIG HIGH']
-standard_out_2 = ['NAO', 'SIM']
-standard_out_3 = ['BAD', 'MEDIUM', 'GOOD']
+    ##############################
+    ##    FUZZY DEFINITION      ##
+    ##############################
+    # MEMBERSHIPS NAME LIST
+
+    standard_in_3 = ['LOW', 'MEDIUM', 'HIGH']
+    special_in_3 = ['LOW', 'OK', 'HIGH']
+    special_inout_3 = ['BAD', 'MEDIUM', 'GOOD']
+    temp_in_3 = ['NEG', 'OK', 'POS']
+    standard_in_5 = ['BIG LOW', 'LOW', 'MEDIUM', 'HIGH', 'BIG HIGH']
+
+    standard_out_3 = ['BAD', 'MEDIUM', 'GOOD']
+
+    ##############################
+    ####  DENSITY
+
+    # MEMBERSHIPS
+    time_var = control.Antecedent(np.arange(0, 1.01, 0.01), 'time_var')
+    density_error = control.Antecedent(np.arange(0, 1.01, 0.01), 'density_error')
+    density_out = control.Consequent(np.arange(0, 1.01, 0.01), 'density')
+
+    # MEMBERSHIP SHAPE
+    time_var.automf(5, 'quant', standard_in_5)
+    density_error.automf(3, 'quant', standard_in_3)
+    density_out.automf(3, 'quant', standard_out_3)
+
+    # RULES
+    rule_density_1 = control.Rule(time_var['BIG LOW'], density_out['GOOD'])
+    rule_density_2 = control.Rule(time_var['LOW'], density_out['GOOD'])
+    rule_density_3 = control.Rule(time_var['MEDIUM'] & density_error['HIGH'], density_out['MEDIUM'])
+    rule_density_4 = control.Rule(time_var['MEDIUM'] & (density_error['MEDIUM'] | density_error['LOW']), density_out['GOOD'])
+    rule_density_5 = control.Rule(time_var['HIGH'] & density_error['HIGH'], density_out['BAD'])
+    rule_density_6 = control.Rule(time_var['HIGH'] & density_error['MEDIUM'], density_out['MEDIUM'])
+    rule_density_7 = control.Rule(time_var['HIGH'] & density_error['LOW'], density_out['GOOD'])
+    rule_density_8 = control.Rule(time_var['BIG HIGH'] & (density_error['HIGH'] | density_error['MEDIUM']), density_out['BAD'])
+    rule_density_9 = control.Rule(time_var['BIG HIGH'] & density_error['LOW'], density_out['MEDIUM'])
+
+    # CONTROL SYSTEM
+    density_control = control.ControlSystem([rule_density_1, rule_density_2, rule_density_3, rule_density_4,
+                                             rule_density_5, rule_density_6, rule_density_7, rule_density_8,
+                                             rule_density_9])
+
+    density = control.ControlSystemSimulation(density_control)
+
+    ##############################
+    ####  COLOR
+
+    # MEMBERSHIPS
+    # TIME_VAR ALREADY CREATED BEFORE IN DENSITY
+    color_error = control.Antecedent(np.arange(0, 1.01, 0.01), 'color_error')
+    color_out = control.Consequent(np.arange(0, 1.01, 0.01), 'color_out')
+
+    # MEMBERSHIP SHAPE
+    # TIME_VAR ALREADY DEFINED BEFORE IN DENSITY
+    color_error.automf(3, 'quant', standard_in_3)
+    color_out.automf(3, 'quant', standard_out_3)
+
+    # RULES
+    rule_color_1 = control.Rule(time_var['BIG LOW'], color_out['GOOD'])
+    rule_color_2 = control.Rule(time_var['LOW'], color_out['GOOD'])
+    rule_color_3 = control.Rule(time_var['MEDIUM'] & color_error['HIGH'], color_out['MEDIUM'])
+    rule_color_4 = control.Rule(time_var['MEDIUM'] & (color_error['MEDIUM'] | color_error['LOW']), color_out['GOOD'])
+    rule_color_5 = control.Rule(time_var['HIGH'] & color_error['HIGH'], color_out['BAD'])
+    rule_color_6 = control.Rule(time_var['HIGH'] & color_error['MEDIUM'], color_out['MEDIUM'])
+    rule_color_7 = control.Rule(time_var['HIGH'] & color_error['LOW'], color_out['GOOD'])
+    rule_color_8 = control.Rule(time_var['BIG HIGH'] & (color_error['HIGH'] | color_error['MEDIUM']), color_out['BAD'])
+    rule_color_9 = control.Rule(time_var['BIG HIGH'] & color_error['LOW'], color_out['MEDIUM'])
+
+    # CONTROL SYSTEM
+    color_control = control.ControlSystem([rule_color_1, rule_color_2, rule_color_3, rule_color_4,
+                                           rule_color_5, rule_color_6, rule_color_7, rule_color_8,
+                                           rule_color_9])
+
+    color = control.ControlSystemSimulation(color_control)
+
+    ##############################
+    ####  TANINS
+    if use_tanins:
+        # MEMBERSHIPS
+        # TIME_VAR ALREADY CREATED BEFORE IN DENSITY
+        tanins_error = control.Antecedent(np.arange(0, 1.01, 0.01), 'tanins_error')
+        tanins_out = control.Consequent(np.arange(0, 1.01, 0.01), 'tanins_out')
+
+        # MEMBERSHIP SHAPE
+        # TIME_VAR ALREADY DEFINED BEFORE IN DENSITY
+        tanins_error.automf(3, 'quant', standard_in_3)
+        tanins_out.automf(3, 'quant', standard_out_3)
+
+        # RULES
+        rule_tanins_1 = control.Rule(time_var['BIG LOW'], tanins_out['GOOD'])
+        rule_tanins_2 = control.Rule(time_var['LOW'], tanins_out['GOOD'])
+        rule_tanins_3 = control.Rule(time_var['MEDIUM'] & tanins_error['HIGH'], tanins_out['MEDIUM'])
+        rule_tanins_4 = control.Rule(time_var['MEDIUM'] & (tanins_error['MEDIUM'] | tanins_error['LOW']), tanins_out['GOOD'])
+        rule_tanins_5 = control.Rule(time_var['HIGH'] & tanins_error['HIGH'], tanins_out['BAD'])
+        rule_tanins_6 = control.Rule(time_var['HIGH'] & tanins_error['MEDIUM'], tanins_out['MEDIUM'])
+        rule_tanins_7 = control.Rule(time_var['HIGH'] & tanins_error['LOW'], tanins_out['GOOD'])
+        rule_tanins_8 = control.Rule(time_var['BIG HIGH'] & (tanins_error['HIGH'] | tanins_error['MEDIUM']), tanins_out['BAD'])
+        rule_tanins_9 = control.Rule(time_var['BIG HIGH'] & tanins_error['LOW'], tanins_out['MEDIUM'])
+
+        # CONTROL SYSTEM
+        tanins_control = control.ControlSystem([rule_tanins_1, rule_tanins_2, rule_tanins_3, rule_tanins_4,
+                                                rule_tanins_5, rule_tanins_6, rule_tanins_7, rule_tanins_8,
+                                                rule_tanins_9])
+
+        tanins = control.ControlSystemSimulation(tanins_control)
+
+    ##############################
+    ####  REMONTAGEM
+
+    # MEMBERSHIPS
+    color_in = control.Antecedent(np.arange(0, 1.01, 0.01), 'color_in')
+    tanins_in = control.Antecedent(np.arange(0, 1.01, 0.01), 'tanins_in')
+    remontagem_out = control.Consequent(np.arange(0, 1.01, 0.01), 'remontagem_out')
+
+    # MEMBERSHIP SHAPE (TANINS NAME DEPEND ON TABLE USE)
+    color_in.automf(3, 'quant', special_inout_3)
+    if use_tanins:
+        tanins_in.automf(3, 'quant', special_inout_3)
+    else:
+        tanins_in.automf(3, 'quant', special_in_3)
+    remontagem_out['NAO'] = fuzz.trapmf(remontagem_out.universe, [0, 0, 0.2, 0.2])
+    remontagem_out['SIM'] = fuzz.trapmf(remontagem_out.universe, [0.8, 0.8, 1, 1])
 
 
-####  DENSITY
+    # RULES (TANINS NAME DEPEND ON TABLE USE)
+    rule_remontagem_1 = control.Rule(color_in['GOOD'], remontagem_out['NAO'])
+    rule_remontagem_2 = control.Rule(color_in['BAD'], remontagem_out['SIM'])
+    if use_tanins:
+        rule_remontagem_3 = control.Rule(color_in['MEDIUM'] & tanins_in['GOOD'], remontagem_out['NAO'])
+        rule_remontagem_4 = control.Rule(color_in['MEDIUM'] & (tanins_in['MEDIUM'] | tanins_in['BAD']), remontagem_out['SIM'])
+    else:
+        rule_remontagem_3 = control.Rule(color_in['MEDIUM'] & tanins_in['HIGH'], remontagem_out['NAO'])
+        rule_remontagem_4 = control.Rule(color_in['MEDIUM'] & (tanins_in['LOW'] | tanins_in['OK']), remontagem_out['SIM'])
 
-#MENBERSHIPS
-time_var = control.Antecedent(np.arange(0, time_end, 0.01), 'time_var')
-density_error = control.Antecedent(np.arange(0, 1, 0.01), 'density_error')
-density_out = control.Consequent(np.arange(0, 1, 0.01), 'density')
 
-#MENBERSHIP SHAPE
-time_var.automf(5, 'quant', standard_in_5)
-density_error.automf(3, 'quant', standard_in_3)
-density_out.automf(3, 'quant', standard_out_3)
+    # CONTROL SYSTEM
+    remontagem_control = control.ControlSystem([rule_remontagem_1, rule_remontagem_2, rule_remontagem_3, rule_remontagem_4])
 
-#RULES
-rule_density_1 = control.Rule(time_var['BIG LOW'], density_out['GOOD'])
-rule_density_2 = control.Rule(time_var['LOW'], density_out['GOOD'])
-rule_density_3 = control.Rule(time_var['MEDIUM'] & density_error['HIGH'], density_out['MEDIUM'])
-rule_density_4 = control.Rule(time_var['MEDIUM'] & (density_error['MEDIUM'] | density_error['LOW']), density_out['GOOD'])
-rule_density_5 = control.Rule(time_var['HIGH'] & density_error['HIGH'], density_out['BAD'])
-rule_density_6 = control.Rule(time_var['HIGH'] & density_error['MEDIUM'], density_out['MEDIUM'])
-rule_density_7 = control.Rule(time_var['HIGH'] & density_error['LOW'], density_out['GOOD'])
-rule_density_8 = control.Rule(time_var['BIG HIGH'] & (density_error['HIGH'] | density_error['MEDIUM']), density_out['BAD'])
-rule_density_9 = control.Rule(time_var['BIG HIGH'] & density_error['LOW'], density_out['MEDIUM'])
+    remontagem = control.ControlSystemSimulation(remontagem_control)
 
-#CONTROL SYSTEM
-density_control = control.ControlSystem([rule_density_1, rule_density_2, rule_density_3, rule_density_4,
-                                         rule_density_5, rule_density_6, rule_density_7, rule_density_8,
-                                         rule_density_9])
+    ##############################
+    ####  CHILL (DUDE)
 
-density = control.ControlSystemSimulation(density_control)
+    # MEMBERSHIPS
+    # COLOR_IN ALREADY CREATED BEFORE IN REMONTAGEM
+    temp_in = control.Antecedent(np.arange(0, 1.01, 0.01), 'temperature_in')
+    density_in = control.Antecedent(np.arange(0, 1.01, 0.01), 'density_in')
+    chiller_out = control.Consequent(np.arange(0, 1.01, 0.01), 'chiller_out')
 
-####  DENSITY
+    # MEMBERSHIP SHAPE
+    # COLOR_IN ALREADY CREATED BEFORE IN REMONTAGEM
+    temp_in.automf(3, 'quant', temp_in_3)
+    density_in.automf(3, 'quant', special_inout_3)
+    chiller_out['NAO'] = fuzz.trapmf(remontagem_out.universe, [0, 0, 0.2, 0.2])
+    chiller_out['SIM'] = fuzz.trapmf(remontagem_out.universe, [0.8, 0.8, 1, 1])
 
-#MENBERSHIPS
-time_var = control.Antecedent(np.arange(0, time_end, 0.01), 'time_var')
-density_error = control.Antecedent(np.arange(0, 1, 0.01), 'density_error')
-density_out = control.Consequent(np.arange(0, 1, 0.01), 'density')
+    # RULES
+    rule_chiller_1 = control.Rule(temp_in['NEG'], chiller_out['NAO'])
+    rule_chiller_2 = control.Rule(temp_in['POS'], chiller_out['SIM'])
+    rule_chiller_3 = control.Rule(temp_in['OK'] & color_in['BAD'] & density_in['BAD'], chiller_out['NAO'])
+    rule_chiller_4 = control.Rule(temp_in['OK'] & color_in['BAD'] & (density_in['MEDIUM'] | density_in['GOOD']), chiller_out['SIM'])
+    rule_chiller_5 = control.Rule(temp_in['OK'] & color_in['MEDIUM'] & density_in['GOOD'], chiller_out['SIM'])
+    rule_chiller_6 = control.Rule(temp_in['OK'] & color_in['GOOD'], chiller_out['NAO'])
 
-#MENBERSHIP SHAPE
-time_var.automf(5, 'quant', standard_in_5)
-density_error.automf(3, 'quant', standard_in_3)
-density_out.automf(3, 'quant', standard_out_3)
+    # CONTROL SYSTEM
+    chiller_control = control.ControlSystem([rule_chiller_1, rule_chiller_2, rule_chiller_3, rule_chiller_4,
+                                             rule_chiller_5, rule_chiller_6])
 
-#RULES
-rule_density_1 = control.Rule(time_var['BIG LOW'], density_out['GOOD'])
-rule_density_2 = control.Rule(time_var['LOW'], density_out['GOOD'])
-rule_density_3 = control.Rule(time_var['MEDIUM'] & density_error['HIGH'], density_out['MEDIUM'])
-rule_density_4 = control.Rule(time_var['MEDIUM'] & (density_error['MEDIUM'] | density_error['LOW']), density_out['GOOD'])
-rule_density_5 = control.Rule(time_var['HIGH'] & density_error['HIGH'], density_out['BAD'])
-rule_density_6 = control.Rule(time_var['HIGH'] & density_error['MEDIUM'], density_out['MEDIUM'])
-rule_density_7 = control.Rule(time_var['HIGH'] & density_error['LOW'], density_out['GOOD'])
-rule_density_8 = control.Rule(time_var['BIG HIGH'] & (density_error['HIGH'] | density_error['MEDIUM']), density_out['BAD'])
-rule_density_9 = control.Rule(time_var['BIG HIGH'] & density_error['LOW'], density_out['MEDIUM'])
+    chiller = control.ControlSystemSimulation(chiller_control)
 
-#CONTROL SYSTEM
-density_control = control.ControlSystem([rule_density_1, rule_density_2, rule_density_3, rule_density_4,
-                                         rule_density_5, rule_density_6, rule_density_7, rule_density_8,
-                                         rule_density_9])
+    ##############################
+    ##   EOF FUZZY DEFINITION   ##
+    ##############################
 
-density = control.ControlSystemSimulation(density_control)
+    if use_tanins:
+        return density, color, tanins, remontagem, chiller
+    else:
+        return density, color, remontagem, chiller
 
-#EXAMPLE SIMULATION
 
-density.input['time_var'] = 8
-density.input['density_error'] = 0.2
-density.compute()
+# THIS IS THE MAIN CONTROL ROUTINE
+def control_routine(time_end, color_end, q):
 
-print(density.output['density'])
-density_out.view(sim=density)
+    ##############################
+    ##     INPUT VARIABLES      ##
+    ##############################
+    # VARIABLES TO BE RECEIVED BY INITIAL CALL OF FUNCTION BY MAIN THREAD
 
-##############################
-##   EOF FUZZY DEFINITION   ##
-##############################
+    # TIME_END   - FINAL TIME GIVEN BY CLIENT
+    # COLOR_END  - FINAL COLOR EXPECTED BY CLIENT
+    # q          - Queue Variable Create One and pass values as such
+    #                       from queue import Queue
+    #                       q = Queue()
+    #                       q.put(density_from_sensor)
+    #                       q.put(color_from_sensor) and so forth (4 times)
+    #                       use this order: density/color/temperature/tanins
+
+
+    ##############################
+    ##     CONTROL VARIABLES    ##
+    ##############################
+    # VARIABLES TO CONTROL IMPLEMENTATION OF FUZZY
+    use_tanins = False      # USE TANINS TABLE OR NOT
+    use_sensor = False      # SIMULATE USING SENSORS OR NOT
+
+
+    ##############################
+    ##     GENERIC VARIABLES    ##
+    ##############################
+    q = Queue()
+
+
+    # INITIALIZE FUZZY CONTROL
+    if use_tanins:
+        density, color, tanins, remontagem, chiller = fuzzy_initialization(use_tanins)
+    else:
+        density, color, remontagem, chiller = fuzzy_initialization(use_tanins)
+
+    while True:
+
+        # WAIT FOR SENSOR INPUT
+        if use_sensor:
+            density_sensor = q.get()
+            color_sensor = q.get()
+            temperature_sensor = q.get()
+            tanins_sensor = q.get()
+
+        # USE THIS TO SIMULATE VALUES
+        else:
+            density_sensor = 0.2
+            color_sensor = 0.4
+            temperature_sensor = 0.3
+            tanins_sensor = 0.7
+
+        calculate_error()
+
 
